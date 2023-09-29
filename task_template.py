7985bebcb11bf5309bbb65b3dd6969be1af6d227
EngetoStudentAdamSimun <adam.simunek@seznam.cz>
2023-09-29 17:37:42 +0200

    select text

--- task_template.py
+++ task_template.py
@@ -1,15 +1,6 @@
-"""
-PROJEKT_1.py: první projekt do Engeto Online Python Akademie
-
-author: Adam Šimůnek
-email: adam.simunek@seznam.cz
-discord: -
-"""
-
-#help
-separator = "-"*54
-
-#entrance variables - texts to be analyzed
+'''
+author =
+'''
 TEXTS = ['''
 Situated about 10 miles west of Kemmerer,
 Fossil Butte is a ruggedly impressive
@@ -39,40 +30,4 @@ garpike and stingray are also present.'''
 ]
 
 
-#entrance variables - login
-log_in = dict()
-log_in["bob"] = "123"
-log_in["ann"] = "pass123"
-log_in["mike"] = "password123"
-log_in["liz"] = "pass123"
-
-#log-in processa
-user_log = input("username: ")
-password_log = input("password: ")
-print(separator)
-
-if user_log in log_in.keys() and log_in[user_log] == password_log:
-    print("Welcome to the app,", user_log, "\nWe have",len(TEXTS), "text to be analyzed")
-    print(separator)    
-else:
-    print("unregistred user, terminating the program")
-    quit()
-
-#select text
-print("Enter a number btw. 1 and ", str(len(TEXTS)))
-selected_number = input("to select: ")
-
-#text index check
-if not int(selected_number) in range(1,len(TEXTS)): 
-    print("selected number does not in range, terminating the program")
-elif selected_number.isnumeric() != True:
-    print("selected number is not numerical, terminating the program")
-else: 
-    print(separator)
-
-
-
-
-
-
-
+print(len(TEXTS))
